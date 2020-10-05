721ba70d4136ee6dab3a3a07688a040ad68c67a8
Thabang Chewa <50702994+tchewaa@users.noreply.github.com>
2020-10-05 09:49:24 +0200

    Update script.sh
    added a zoom script

--- script.sh
+++ script.sh
@@ -4,9 +4,13 @@
 apt-get update
 
 echo "installing spotify"
-snap install spotify
+snap install spotify -y
 echo "spotify installation complete"
 
 echo "installing vim"
 sudo apt install vim -y
 echo "vim installation complete"
+
+echo "installing zoom"
+sudo snap install zoom-client -y
+echo "zoom installation complete"
